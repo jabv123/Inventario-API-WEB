7edbec4c4f7205998c7ad602491c3c3630253681
Neid-09 <neiderqm09@gmail.com>
2025-04-20 18:39:11 -0500

    Create Servicio de ImgProducto

---
+++ src/main/java/org/apirest/service/ImgProductoService.java
@@ -0,0 +1,54 @@
+package org.apirest.service;
+
+import java.util.List;
+
+import org.apirest.modelo.ImgProducto;
+import org.apirest.repository.ImgProductoRepo;
+
+public class ImgProductoService {
+
+    private final ImgProductoRepo imgProductoRepo;
+
+    public ImgProductoService(ImgProductoRepo imgProductoRepo){
+        this.imgProductoRepo = imgProductoRepo;
+    } 
+
+    //Listar todas la imagenes
+    public List<ImgProducto> getImgsProductos(){
+        return imgProductoRepo.getIgms();
+    }
+
+    //Listar imgs por id
+    public ImgProducto getImgById(int id){
+        return imgProductoRepo.getByIdImg(id);
+    }
+
+    //Listar todas las imagenes por producto(id)
+    public List<ImgProducto> getImgByProducto(int idProducto){
+        return imgProductoRepo.getImgsByIdProducto(idProducto);
+    }
+
+    //Crear imgs para producto(se asigna el producto por el idProducto en peticion)
+    public ImgProducto agregarImgProducto(ImgProducto img){
+        return imgProductoRepo.agrgarImg(img);
+    }
+
+    //Actualizar img
+    public ImgProducto actualizarImgProducto(ImgProducto img){
+        return imgProductoRepo.actualizarImg(img);
+    }
+
+    //Eliminar img por id
+    public void eliminarImg(int id){
+        imgProductoRepo.eliminarImg(id);
+    }
+
+    //Eliminar img por idProducto
+    public boolean eliminarImgByProducto(int idProducto){
+        return imgProductoRepo.eliminarImgsByIdProducto(idProducto);
+    }
+
+
+
+
+}
