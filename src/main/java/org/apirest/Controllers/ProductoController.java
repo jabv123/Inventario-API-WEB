7b82f94259cd696de55ec9e00d296d5cd47357f5
jabv123 <andresbotina97@gmail.com>
2025-04-20 15:17:01 -0500

    test the error

--- src/main/java/org/apirest/Controllers/ProductoController.java
+++ src/main/java/org/apirest/Controllers/ProductoController.java
@@ -2,9 +2,7 @@ package org.apirest.Controllers;
 
 import org.apirest.Controllers.ProductoController;
 import org.apirest.modelo.Mensaje;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 
 public class ProductoController {
 }
