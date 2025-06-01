bfd64057625de02f5818603dd5fa40bca2680ef3
Neid-09 <neiderqm09@gmail.com>
2025-06-01 18:08:45 -0500

    Añadir clase LogSistema para gestionar registros del sistema con atributos como fecha, mensaje, idReferencia de quien hace un registro  y el nombre.

---
+++ src/main/java/org/apirest/modelo/LogSistema.java
@@ -0,0 +1,56 @@
+package org.apirest.modelo;
+
+import java.time.LocalDateTime;
+
+public class LogSistema {
+
+    private int id;
+    private LocalDateTime fecha;
+    private String mensaje;
+    private int idReferencia;
+    private String nombre;
+
+    public LogSistema(){}
+
+    public LogSistema(LocalDateTime fecha, String mensaje, int idReferencia, String nombre) {
+        this.fecha = fecha;
+        this.mensaje = mensaje;
+        this.idReferencia = idReferencia;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public int getIdReferencia() {
+        return idReferencia;
+    }
+    public void setIdReferencia(int idReferencia) {
+        this.idReferencia = idReferencia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
